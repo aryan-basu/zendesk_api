b0233b44c5558b4decb47de7792a4a07ee11e0aa
aryan basu <aryanbasu005@gmail.com>
2024-10-29 12:08:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ data = [
         "email": "aryan.basu@ptw.com",
     }
 ];
+app.use(cors())
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
